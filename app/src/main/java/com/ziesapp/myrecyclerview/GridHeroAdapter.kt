d353c5b74e0fc9ece0adbafd82ade37e779a6f80
Alief Azies <Alief.ahmadazies@gmail.com>
2020-05-27 15:50:55 +0700

    penambahan layout dan kode untuk menu grid

---
+++ app/src/main/java/com/ziesapp/myrecyclerview/GridHeroAdapter.kt
@@ -0,0 +1,23 @@
+package com.ziesapp.myrecyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GridHeroAdapter(val listPahlawan: ArrayList<Pahlawan>) :
+    RecyclerView.Adapter<GridHeroAdapter.GridViewHolder> {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): GridHeroAdapter.GridViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GridHeroAdapter.GridViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
